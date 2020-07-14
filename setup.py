fc922b94a71764ccc49517f9c62676e2d55a4f74
raul <rauldearriba@gmail.com>
2020-07-14 10:04:09 +0200

    fix code errors

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+import setuptools
 from distutils.core import setup
 
-setup(name='Distutils',
-      version='1.0',
-      description='Software for shops',
-      author='Raül de Arriba',
-      author_email='rauldearriba@gmail.com',
-      url='https://www.radega.com/',
-      packages=['distutils', 'distutils.command'],
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='CodeStructureMaker_v1',
+    version='1.0',
+    description='Software to generade structures',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Raül de Arriba',
+    author_email='rauldearriba@gmail.com',
+    url='https://github.com/Radega1993/CodeStructureMaker',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
